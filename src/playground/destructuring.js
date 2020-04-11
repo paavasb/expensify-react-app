d1a37b164c3de6e6528e67dd515fd12530af12fa
Paavas Bhasin <paavas22@gmail.com>
2020-04-11 15:17:47 +0530

    Lecture 89

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object
+//
+
 // const person = {
 //     name: 'Paavas',
 //     age: 20,
@@ -18,14 +22,27 @@
 //     console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {name: publisherName = 'Self Published'} = book.publisher;
+// const {name: publisherName = 'Self Published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+
+//
+// Array
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+const [, city, state = 'New York'] = address;
+console.log(`You are in ${city}, ${state}.`)
+
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [description, , mediumCost] = item;
+console.log(`A medium ${description} costs ${mediumCost}.`);
