53c79cb9c27f0cd1c3a099c271530bdb79ec402d
Paavas Bhasin <paavas22@gmail.com>
2020-04-11 20:06:53 +0530

    Lecture 92

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,42 @@
 import { createStore, combineReducers } from 'redux';
 
+
+// Expenses Reducers
+
+const expensesReducersDefaultState = [];
+
+const expensesReducers = (state = expensesReducersDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filter Reducers 
+
+const filtersReducersDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducers = (state = filtersReducersDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducers,
+        filters: filtersReducers
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expense: [{
         id: 'psof',
