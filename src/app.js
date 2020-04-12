a538584ea34d847233eb99d374608eef6aff869d
Paavas Bhasin <paavas22@gmail.com>
2020-04-12 20:18:12 +0530

    Lecture 101

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -12,12 +13,22 @@ const store = configureStore();
 
 store.dispatch(addExpense({description: 'Water Bill', amount: 2000, createdAt: 10000}));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 1500, createdAt: 2000}));
-store.dispatch(setTextFilter('bill'));
-// store.dispatch(setTextFilter('water'));
+// store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
