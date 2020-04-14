c808d964d21c28dc373582ffdad72a974ceb0d8e
Paavas Bhasin <paavas22@gmail.com>
2020-04-14 21:26:47 +0530

    Lecture 117

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,75 @@
+import expensesReducers from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+import moment from 'moment';
+
+test('should set default state', () => {
+    const state = expensesReducers(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducers(expenses, action);
+    expect(state).toEqual([ expenses[0], expenses[2] ]);
+});
+
+test('should not remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducers(expenses, action);
+    expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+    const expense = {
+        id: '4',
+        description: 'Music',
+        note: 'Apple',
+        amount: 454,
+        createdAt: moment(0).valueOf()
+    };
+    const action = {
+        type: 'ADD_EXPENSE',
+        expense
+    };
+    const state = expensesReducers(expenses, action);
+    expect(state).toEqual([...expenses, expense]);
+});
+
+test('should edit an expense', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: expenses[1].id,
+        updates: {
+            note: 'Elliston Place',
+            amount: 100000
+        }
+    };
+    const newExpense = {
+        id: '2',
+        description: 'Rent',
+        note: 'Elliston Place',
+        amount: 100000,
+        createdAt: moment(0).subtract(4, 'days').valueOf()
+    };
+    const state = expensesReducers(expenses, action);
+    expect(state).toEqual([ expenses[0], newExpense, expenses[2] ]);
+});
+
+test('should edit an expense', () => {
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: '-1',
+        updates: {
+            note: 'Elliston Place',
+            amount: 100000
+        }
+    };
+    const state = expensesReducers(expenses, action);
+    expect(state).toEqual(expenses);
+});
