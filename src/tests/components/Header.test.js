2f45f61ed5a63c22e7eda6f61af293c97c46370d
Paavas Bhasin <paavas22@gmail.com>
2020-04-14 23:58:56 +0530

    Lecture 119

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-    //console.log(renderer.getRenderOutput());
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // //console.log(renderer.getRenderOutput());
 })
