3186cb0152b461350b39dae1fd3ee3e34f475e7f
Paavas Bhasin <paavas22@gmail.com>
2020-04-18 00:15:20 +0530

    Lecture 150

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,19 +15,57 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref('expenses').push({
-    description: 'Gum',
-    note: '',
-    amount: 195,
-    createdAt: 0
+database.ref('expenses').on('child_removed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
 });
 
-database.ref('expenses').push({
-    description: 'Rent',
-    note: '',
-    amount: 109500,
-    createdAt: 1000
+database.ref('expenses').on('child_changed', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
 });
+
+database.ref('expenses').on('child_added', (snapshot) => {
+    console.log(snapshot.key, snapshot.val());
+});
+
+// database.ref('expenses')
+//   .on('value', (snapshot) => {
+//       const expenses = [];
+//       snapshot.forEach((childSnapshot) => {
+//           expenses.push({
+//               id: childSnapshot.key,
+//               ...childSnapshot.val()
+//           });
+//       });
+//       console.log(expenses);
+// });
+
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//       const expenses = [];
+//       snapshot.forEach((childSnapshot) => {
+//           expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//           });
+//       });
+
+//       console.log(expenses);
+//   });
+
+// database.ref('expenses').push({
+//     description: 'Gum',
+//     note: '',
+//     amount: 195,
+//     createdAt: 0
+// });
+
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 109500,
+//     createdAt: 1000
+// });
 
 database.ref('expenses').push({
     description: 'Credit Card',
