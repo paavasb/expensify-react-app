01fff2c59757f25025edf00e4b07294c36a0f7f1
Paavas Bhasin <paavas22@gmail.com>
2020-04-19 16:38:01 +0530

    Lecture 155

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYSzQ2Jv_Y23sMNn38G90DlqZ4Gvln7yI",
-    authDomain: "expensify-53c16.firebaseapp.com",
-    databaseURL: "https://expensify-53c16.firebaseio.com",
-    projectId: "expensify-53c16",
-    storageBucket: "expensify-53c16.appspot.com",
-    messagingSenderId: "777555561747",
-    appId: "1:777555561747:web:565b7e09e2edf7442adee6",
-    measurementId: "G-455TH2S1CT"
+    apiKey: process.env.FIREBASE_API_KEY, 
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
 firebase.initializeApp(firebaseConfig);
