4fb9ea1313d2367759440fe7138f595f5830a6f6
Paavas Bhasin <paavas22@gmail.com>
2020-04-19 12:49:24 +0530

    Lecture 153

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,10 +1,10 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'Paavas',
-        //     age: 20
-        // });
-        reject('Something went wrong');
+        resolve({
+            name: 'Paavas',
+            age: 20
+        });
+        //reject('Something went wrong');
     }, 5000);
 });
 
@@ -12,6 +12,9 @@ console.log('before');
 
 promise.then((data) => {
     console.log('1', data);
+    return 'some data';
+}).then((str) => {
+    console.log('does this run?', str);
 }).catch((error) => {
     console.log('error: ', error);
 });
